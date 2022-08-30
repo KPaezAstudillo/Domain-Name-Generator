7e35af840b63768a3b1e6606503afc79a6417439
KPaezAstudillo <karinapaezastudillo@gmail.com>
2022-08-30 21:45:07 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#domain").innerHTML = generateDomain();
+  });
 };
+
+function generateDomain() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dmain = [".com", ".cl", ".net"];
+  let myDomain = [];
+  let list = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dmain.length; l++) {
+          myDomain = pronoun[i] + adj[j] + noun[k] + dmain[l];
+          list = myDomain + " <br>" + list;
+        }
+      }
+    }
+  }
+  return list;
+}
